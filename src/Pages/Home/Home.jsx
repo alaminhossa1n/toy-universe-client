0b06752add95c198f8f00c716a1e9b4f79154b2f
alaminhossa1n <alaminatghoria@gmail.com>
2023-05-21 22:52:51 +0600

    img gallery added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,9 +5,13 @@ import { useEffect, useState } from 'react';
 import ToyCard from './ToyCard';
 import Banner from './Banner';
 import useTitle from '../../hooks/TitleHooks';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
-    useTitle('Toy - Home')
+    useTitle('Toy - Home');
+
+    const loadedData = useLoaderData();
+    const galleryImg = loadedData.slice(0, 9);
 
     const [selectedToys, setSelectedToys] = useState([]);
 
@@ -45,50 +49,76 @@ const Home = () => {
 
         <div className=''>
             <Banner></Banner>
-            <Tabs className="py-8">
-                <TabList className="flex justify-center gap-10">
-                    <Tab onClick={() => handleActionFigures('Action Figures')} className="rounded py-2 px-4 text-sm font-medium text-gray-500">
-                        <p>Action Figures</p>
-                    </Tab>
-                    <Tab onClick={() => handlePlushToys('Plush Toys')} className="py-2 px-4 text-sm font-medium text-gray-500">
-                        <p>Plush Toys</p>
-                    </Tab>
-                    <Tab onClick={() => handleGamesAndPuzzles('Games and Puzzles')} className="py-2 px-4 text-sm font-medium text-gray-500">
-                        <p>Games and Puzzles</p>
-                    </Tab>
-                </TabList>
+            {/* gallery */}
 
-                <TabPanel>
-                    <div className="p-6 grid md:grid-cols-3 gap-5">
-                        {
-                            selectedToys.map(toy => <ToyCard
-                                key={toy._id}
-                                toy={toy}
-                            ></ToyCard>)
-                        }
-                    </div>
-                </TabPanel>
-                <TabPanel>
-                    <div className="p-6 grid md:grid-cols-3 gap-5">
-                        {
-                            selectedToys.map(toy => <ToyCard
-                                key={toy._id}
-                                toy={toy}
-                            ></ToyCard>)
-                        }
-                    </div>
-                </TabPanel>
-                <TabPanel>
-                    <div className="p-6 grid md:grid-cols-3 gap-5">
-                        {
-                            selectedToys.map(toy => <ToyCard
-                                key={toy._id}
-                                toy={toy}
-                            ></ToyCard>)
-                        }
-                    </div>
-                </TabPanel>
-            </Tabs>
+            <section>
+                <div className='text-center'>
+                    <h2 className='text-5xl font-bold py-5 text-[#37306B]'>Image Gallery</h2>
+                </div>
+                <div className='grid md:grid-cols-3 gap-5 w-3/4 mx-auto'>
+
+                    {
+                        galleryImg.map(img => <img
+                        className='shadow rounded-lg'
+                            key={img._id}
+                            src={img.pictureUrl}
+
+                        />)
+                    }
+                </div>
+            </section>
+
+
+            {/* shop by category */}
+            <section>
+                <div className='text-center'>
+                    <h2 className='text-5xl font-bold py-5 text-[#37306B]'>Shop By Category</h2>
+                </div>
+                <Tabs className="py-8">
+                    <TabList className="flex justify-center gap-10">
+                        <Tab onClick={() => handleActionFigures('Action Figures')} className="rounded py-2 px-4 text-sm font-medium text-gray-500">
+                            <p>Action Figures</p>
+                        </Tab>
+                        <Tab onClick={() => handlePlushToys('Plush Toys')} className="py-2 px-4 text-sm font-medium text-gray-500">
+                            <p>Plush Toys</p>
+                        </Tab>
+                        <Tab onClick={() => handleGamesAndPuzzles('Games and Puzzles')} className="py-2 px-4 text-sm font-medium text-gray-500">
+                            <p>Games and Puzzles</p>
+                        </Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <div className="p-6 grid md:grid-cols-3 gap-5">
+                            {
+                                selectedToys.map(toy => <ToyCard
+                                    key={toy._id}
+                                    toy={toy}
+                                ></ToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="p-6 grid md:grid-cols-3 gap-5">
+                            {
+                                selectedToys.map(toy => <ToyCard
+                                    key={toy._id}
+                                    toy={toy}
+                                ></ToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="p-6 grid md:grid-cols-3 gap-5">
+                            {
+                                selectedToys.map(toy => <ToyCard
+                                    key={toy._id}
+                                    toy={toy}
+                                ></ToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
+                </Tabs>
+            </section>
         </div>
     );
 };
